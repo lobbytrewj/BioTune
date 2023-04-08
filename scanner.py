c7f9426d169f11892f7d3a015ab27cff80ee6939
Hyunseok Jeong <hjeong959@student.fuhsd.org>
2023-04-08 14:52:51 -0700

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,10 +1,15 @@
 import cv2
 import pyzbar
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 # Define a function to read barcodes from a video stream
 def read_barcodes(camera):
     # Initialize video capture object for the camera
     cap = cv2.VideoCapture(camera)
+    # Initialize web driver for Amazon search
+    driver = webdriver.Chrome()
+    driver.get("https://www.amazon.com/")
     # Loop over frames from the video stream
     while True:
         # Read a frame from the video stream
@@ -12,7 +17,7 @@ def read_barcodes(camera):
         # Convert the frame to grayscale
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         # Find barcodes in the grayscale frame
-        barcodes = pyzbar.decode(gray)
+        barcodes = decode(gray)
         # Loop over detected barcodes
         for barcode in barcodes:
             # Extract the barcode data as a byte string
@@ -22,6 +27,21 @@ def read_barcodes(camera):
             # Print the product name and manufacturer
             print("Product Name: ", product_name)
             print("Manufacturer: ", manufacturer)
+            # Search for the product on Amazon
+            search_input = driver.find_element_by_id("twotabsearchtextbox")
+            search_input.send_keys(product_name + " " + manufacturer)
+            search_input.send_keys(Keys.RETURN)
+            # Wait for the search results to load
+            driver.implicitly_wait(10)
+            # Get the title and price of the first search result
+            try:
+                first_result_title = driver.find_element_by_css_selector(".s-result-item h2 a").text
+                first_result_price = driver.find_element_by_css_selector(".s-result-item .a-price-whole").text
+                # Print the title and price
+                print("First Result Title: ", first_result_title)
+                print("First Result Price: $", first_result_price)
+            except:
+                print("No results found")
         # Show the video stream in a window
         cv2.imshow("Barcode Scanner", frame)
         # Wait for a key press
@@ -29,9 +49,10 @@ def read_barcodes(camera):
         # Exit the loop if the 'q' key is pressed
         if key == ord("q"):
             break
-    # Release the video capture object and close the window
+    # Release the video capture object, close the window, and quit the web driver
     cap.release()
     cv2.destroyAllWindows()
+    driver.quit()
 
 # Call the read_barcodes function with the camera index as an argument
 read_barcodes(0)
