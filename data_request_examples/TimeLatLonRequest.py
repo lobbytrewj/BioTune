6fda6c75f9c030135cd0b1e486b6d0de65fff74c
michaelfenggg <mfenggg06@gmail.com>
2023-04-08 23:53:53 -0700

    added biomood calculator

--- data_request_examples/TimeLatLonRequest.py
+++ data_request_examples/TimeLatLonRequest.py
@@ -38,8 +38,8 @@ while(json_string[longIndex] != '"'):
     longitude += json_string[longIndex]
     longIndex += 1
 
-print(latitude, longitude) 
-print(input1)
+#print(latitude, longitude) 
+#print(input1)
 
 payload = json.dumps(
     {
@@ -68,6 +68,7 @@ air_temperature = ""
 precipitation_rate = ""
 total_cloud_cover = ""
 relative_humidity = ""
+#print(spire_string)
 while(spire_string[windSpeedIndex] != '}'):
     wind_speed += spire_string[windSpeedIndex]
     windSpeedIndex += 1
@@ -84,6 +85,12 @@ while(spire_string[humidityIndex] != ','):
     relative_humidity += spire_string[humidityIndex]
     humidityIndex += 1
 
+wind_speed = float(wind_speed)
+air_temperature = float(air_temperature)
+precipitation_rate = float(precipitation_rate)
+total_cloud_cover = float(total_cloud_cover)
+relative_humidity = float(relative_humidity)
+
 # print(response1.text)
 # print("Wind speed:",wind_speed)
 # print("temperature:", air_temperature)
@@ -91,6 +98,63 @@ while(spire_string[humidityIndex] != ','):
 # print("Cloud cover:", total_cloud_cover)
 # print("humidity:",relative_humidity)
 
+sleep = 4
+health = 1
+nostress = 4
+celsius = air_temperature - 273.15
+print("total cloud cover " + str(total_cloud_cover))
 
-airTempF = (1.8 * (float(air_temperature)-273.15)) + 32
-print(airTempF)
+moodE = 0
+moodE += sleep * -0.007
+print("Your BioMood after sleep is " + str(moodE))
+moodE += health * 0.135
+print("Your BioMood after health is " + str(moodE))
+moodE += nostress * 0.177
+print("Your BioMood after nostress is " + str(moodE))
+moodE += celsius * 0.003
+print("Your BioMood after temp is " + str(moodE))
+moodE += relative_humidity * 0.002
+print("Your BioMood after humidity is " + str(moodE))
+moodE += wind_speed * 0.007
+print("Your BioMood after humidity is " + str(moodE))
+moodE += (1 - total_cloud_cover/100) * 0.095
+print("Your BioMood after sunshine is " + str(moodE))
+moodE += precipitation_rate * 0.007
+print("Your BioMood after rain is " + str(moodE))
+moodS = 0
+moodS += sleep * 0.029
+print("Your BioMood after sleep is " + str(moodS))
+moodS += health * 0.024
+print("Your BioMood after health is " + str(moodS))
+moodS += nostress * 0.190
+print("Your BioMood after nostress is " + str(moodS))
+moodS += celsius * -0.001
+print("Your BioMood after temp is " + str(moodS))
+moodS += relative_humidity * -0.0003
+print("Your BioMood after humidity is " + str(moodS))
+moodS += wind_speed * -0.000001
+print("Your BioMood after wind is " + str(moodS))
+moodS += (1 - total_cloud_cover/100) * 0.019
+print("Your BioMood after sunshine is " + str(moodS))
+moodS += precipitation_rate * 0.0005
+print("Your BioMood after rain is " + str(moodS))
+moodS *= 100
+moodE *= 100
+print("Energy mood is " + str(moodE) + " and sadness mood is " + str(moodS))
+bioMood = (moodE - moodS)
+print("Overall BioMood is " + str(bioMood))
+
+'''
+print("celsius temp " + str(celsius))
+
+mood = 0
+mood += wind_speed * -1
+print("Your BioMood after wind speed is " + str(mood))
+mood += celsius * -2 + 50
+print("Your BioMood after temp is " + str(mood))
+mood += precipitation_rate * -.05
+print("Your BioMood after rain is " + str(mood))
+mood += (total_cloud_cover - 50) * 1
+print("Your BioMood after cloud covering is " + str(mood))
+mood += relative_humidity * -.05
+print("Your BioMood after humidity is " + str(mood))'''
