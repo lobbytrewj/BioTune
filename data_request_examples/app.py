a8d80ee8c5664aa51529ba07480f7c163caae37d
Hyunseok Jeong <47511916+lobbytrewj@users.noreply.github.com>
2023-04-09 18:04:04 -0700

    k

--- data_request_examples/app.py
+++ data_request_examples/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from TimeLatLonRequest import TimeLatLonRequest
+import TimeLatLonRequest as Body
 
 app = Flask(__name__)
 CORS(app)
@@ -14,9 +15,14 @@ def send_data():
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     return response, 200
+if __name__ == '__main__':
+    app.run(debug=True, port=3000)
+
 
 @app.route('/playlist-url', methods=['GET'])
 def get_playlist_url():
+    ##url = TimeLatLonRequest.get_playlist_url() # Assuming you have created a function to get the playlist URL
+    ##return jsonify({'url': url})
     url = TimeLatLonRequest.get_playlist_url() # Assuming you have created a function to get the playlist URL
     response = jsonify({'url': url})
     response.headers.add('Access-Control-Allow-Origin', '*')
